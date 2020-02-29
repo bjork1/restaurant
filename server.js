ea75975465763c3f157faf3ec8c6dbd9440df743
Steve Kindt <stevemkindt@gmail.com>
2020-02-29 12:39:05 -0600

    haha whoops

--- server.js
+++ server.js
@@ -27,6 +27,18 @@ $(".submit").on("click", function() {
   ];
 });
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
